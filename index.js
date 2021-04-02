2d92df4ac9c4221b39c6acc176993d13bde3cae9
Oroko <aoroko09@gmail.com>
2021-04-01 23:09:58 -0400

    Add an array to hold an object of todos

--- index.js
+++ index.js
@@ -1,24 +1,52 @@
+
+// This is the array that will hold the todolist items
+let todoItems = [] 
+
+// This function will create a new todo object
+// based on the text that was entered in the text input
+// and push it into the 'todoItems' array
+
+
+const addTodo = (text) => {
+	const todo = {
+		text,
+		completed: false,
+		priority: ['Highest','High', 'Medium', 'Low'],
+		id: Date.now()
+	}
+
+	todoItems.push(todo);
+	console.log(todoItems)
+}
+
+// select the form
 const form = document.querySelector('form')
-const input = document.querySelector('#task')
-const ul = document.querySelector('ul')
-
-const listItem = () => {
-	let li = document.createElement('li')
-	li.textContent = input.value
-	ul.appendChild(li)
-}
-
-const clearInput = () => {
-	input.value = ''
-}
 
 
+
+
+
+
+// Add a submit Event listener
 form.addEventListener('submit', (e) => {
 
-	
-	listItem()
-	clearInput()
-	
+	// prevent page from refresh on form submission
 	e.preventDefault()
+
+	// select the text input 
+	const input = document.querySelector('#task')
+
+	// Get the value of the text and remove whitespace
+	const text = input.value.trim();
+
+
+	if (text !== '') {
+		addTodo(text);
+	}
+	input.value = ''
+	input.focus();
+
+
+	
 })
 
